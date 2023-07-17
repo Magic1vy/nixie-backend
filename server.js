b8b7f0ded485c324c65affdbc0e376bd4c38d0a1
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-07-17 02:34:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const routes = require('./ReviewsRoutes');
 const cors = require('cors');
+const helmet = require('helmet');
 
 require("dotenv").config();
 
@@ -19,7 +20,19 @@ mongoose
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors());
-  
+
+app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'"],
+        connectSrc: ["'self'"],
+        reportUri: '/report-violation',
+      },
+    },
+  }));
 
 app.use(routes)
 
